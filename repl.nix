b2cca3a610063f0230a90207edc8b020097808d4
mlyxshi <mlyxdev@gmail.com>
2022-12-08 06:39:47 -0800

    clean

--- repl.nix
+++ repl.nix
@@ -1,13 +1,12 @@
-# To use this file run "nix repl repl.nix" or,
 let
   flake = builtins.getFlake (toString ./.);
   nixpkgs = flake.inputs.nixpkgs;
-  system = builtins.currentSystem;
-  pkgs = import nixpkgs { inherit system; };
+  pkgs = import nixpkgs { system = builtins.currentSystem; };
   lib = pkgs.lib;
 in
-{
-  inherit flake nixpkgs pkgs lib;
-  inherit (flake) nixosModules overlay;
-  packages = flake.packages.${system};
-}
+{ }
+
+// builtins
+// pkgs
+// lib
+// flake
