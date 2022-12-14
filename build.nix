a19ae5e2b18b88f242cad8c292035bc50a5e2b1d
mlyxshi <mlyxdev@gmail.com>
2022-12-13 19:56:54 -0800

    test zram

--- build.nix
+++ build.nix
@@ -60,4 +60,19 @@ in
       -device virtio-rng-pci \
       -bios ${pkgs.OVMF.fd}/FV/OVMF.fd 
   '';
+
+  # Fast Test without Install 
+  system.build.test0 = pkgs.writeShellScriptBin "test-vm" ''
+    test -f disk.img || ${pkgs.qemu_kvm}/bin/qemu-img create -f qcow2 disk.img 10G
+    exec ${pkgs.qemu_kvm}/bin/qemu-kvm -name ${config.networking.hostName} \
+      -m 2048 \
+      -kernel ${config.system.build.kernel}/${kernelTarget}  -initrd ${config.system.build.initialRamdisk}/initrd.zst  \
+      -append "console=ttyS0 init=/bin/init ${toString config.boot.kernelParams} \
+      -no-reboot -nographic \
+      -net nic,model=virtio \
+      -net user,net=10.0.2.0/24,host=10.0.2.2,dns=10.0.2.3,hostfwd=tcp::2222-:22 \
+      -drive file=disk.img,format=qcow2,if=virtio \
+      -device virtio-rng-pci \
+      -bios ${pkgs.OVMF.fd}/FV/OVMF.fd 
+  '';
 }
