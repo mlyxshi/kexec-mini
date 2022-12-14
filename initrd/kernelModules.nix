8c4f6db6da54015f63a40edbed81c00ee21aec19
mlyxshi <mlyxdev@gmail.com>
2022-12-13 19:51:58 -0800

    test zram

--- initrd/kernelModules.nix
+++ initrd/kernelModules.nix
@@ -11,4 +11,9 @@ in
   boot.initrd.kernelModules = modules;
   # NixOS also include default kernel modules: https://github.com/NixOS/nixpkgs/blob/660e7737851506374da39c0fa550c202c824a17c/nixos/modules/system/boot/kernel.nix#L214
   # boot.initrd.includeDefaultModules = false;
+
+
+  boot.extraModprobeConfig = ''
+    options zram num_devices=1
+  '';
 }
