5801f3938c5e4e5688bae822449d42045bcd6b46
mlyxshi <mlyxdev@gmail.com>
2022-12-26 20:58:10 -0800

    test hydra

--- build.nix
+++ build.nix
@@ -27,6 +27,7 @@ let
   kexecScript-x86_64 = pkgs.writeTextDir "script/kexec" (''
     #!/usr/bin/env bash
     set -e   
+
     echo "Downloading kexec-musl-bin" && curl -LO https://hydra.mlyxshi.com/job/kexec/build/x86_64/latest/download-by-type/file/kexec-bin && chmod +x ./kexec-bin
     echo "Downloading initrd" && curl -LO https://hydra.mlyxshi.com/job/kexec/build/x86_64/latest/download-by-type/file/initrd
     echo "Downloading kernel" && curl -LO https://hydra.mlyxshi.com/job/kexec/build/x86_64/latest/download-by-type/file/kernel
@@ -34,7 +35,8 @@ let
 
   kexecScript-aarch64 = pkgs.writeTextDir "script/kexec" (''
     #!/usr/bin/env bash
-    set -e   
+    set -e  
+     
     echo "Downloading kexec-musl-bin" && curl -LO https://hydra.mlyxshi.com/job/kexec/build/aarch64/latest/download-by-type/file/kexec-bin && chmod +x ./kexec-bin
     echo "Downloading initrd" && curl -LO https://hydra.mlyxshi.com/job/kexec/build/aarch64/latest/download-by-type/file/initrd
     echo "Downloading kernel" && curl -LO https://hydra.mlyxshi.com/job/kexec/build/aarch64/latest/download-by-type/file/kernel
